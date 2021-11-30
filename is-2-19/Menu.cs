1067bccb006abcc440627670692482e020cc8495
咯了 大捏了 <nays.tip@mail.ru>
2021-11-30 17:27:33 +0500

    ex

--- is-2-19/Menu.cs
+++ is-2-19/Menu.cs
@@ -52,24 +52,25 @@ namespace is_2_19
             {
                 MessageBox.Show($"Частота = {Chastota}, Количество ядер = {KolYder}, Количество потоков {KolPoto}");
             } 
-            class GPU : CPU
+           
+        }
+        class GPU : CPU
+        {
+            public double ChastotaGPU { get; set; }
+            public string Proizv { get; set; }
+            public double Obeim { get; set; }
+
+            public GPU(double chastotagpu, string proizv, double obeim, double chastota, double kolyder, double kolpoto, double tsena, string year, T articul) : base(chastota, kolyder, kolpoto, tsena, year, articul)
             {
-                public double ChastotaGPU { get; set; }
-                public string Proizv { get; set; }
-                public double Obeim { get; set; }
+                ChastotaGPU = chastotagpu;
+                Proizv = proizv;
+                Obeim = obeim;
+            }
 
-                public GPU(double chastotagpu, string proizv, double obeim, double chastota, double kolyder, double kolpoto, double tsena, string year, T articul) : base(chastota, kolyder, kolpoto, tsena, year, articul)
-                {
-                    ChastotaGPU = chastotagpu;
-                    Proizv = proizv;
-                    Obeim = obeim;
-                }
-
-                public override void Display()
-                {
-                    base.Display();
-                    MessageBox.Show($"Цена данного предмета{Tsena}, год выпуска {Year}, Частота = {Chastota}, Количество ядер = {KolYder}, Количество потоков {KolPoto}, Частота видеокарты{ChastotaGPU}, Производитель{Proizv}, Объем памяти{Obeim}");
-                }
+            public override void Display()
+            {
+                base.Display();
+                MessageBox.Show($"Цена данного предмета{Tsena}, год выпуска {Year}, Частота = {Chastota}, Количество ядер = {KolYder}, Количество потоков {KolPoto}, Частота видеокарты{ChastotaGPU}, Производитель{Proizv}, Объем памяти{Obeim}");
             }
         }
         public Form1()
