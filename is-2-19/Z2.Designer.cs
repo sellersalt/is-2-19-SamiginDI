d1d51db15050c746479ed611757af27e46ab69aa
咯了 大捏了 <nays.tip@mail.ru>
2021-11-30 19:21:44 +0500

    z2

--- is-2-19/Z2.Designer.cs
+++ is-2-19/Z2.Designer.cs
@@ -29,19 +29,34 @@ namespace is_2_19
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "boom";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 381);
+            this.ClientSize = new System.Drawing.Size(234, 200);
+            this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Задание 2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- is-2-19/Z2.cs
+++ is-2-19/Z2.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace is_2_19
 {
     public partial class Form2 : Form
     {
+        
+        
         public Form2()
         {
             InitializeComponent();
+        }
+        class Pod
+        {
+            public static string DV()
+            {
+             const string Host = "caseum.ru";
+             const int Port = 33333;
+             const string Polz = "test_user";
+             const string DB = "db_test";
+             const string Pass = "test_pass";
+
+            string connStr = $"server={Host};port={Port};user={Polz};" + $"database={DB};password={Pass};";
+            return connStr;
+            }
+        }
+        MySqlConnection conn new MySqlConnection(Pod.DV());
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conn = Open();
+                MessageBox.Show("Dobro");
+            }
+            catch
+            {
+                MessageBox.Show("Zlo");
+            }
         }
     }
 }
